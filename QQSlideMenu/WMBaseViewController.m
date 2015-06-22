ca4fb695aad1774a95d2fc45f11f77bdb671df70
WAMaker <wamakerdevelop@gmail.com>
2015-06-22 22:58:37 +0800

    提交丢失的WMBaseViewController.m

---
+++ QQSlideMenu/WMBaseViewController.m
@@ -0,0 +1,16 @@
+//
+//  WMBaseViewController.m
+//  QQSlideMenu
+//
+//  Created by wamaker on 15/6/22.
+//  Copyright (c) 2015年 wamaker. All rights reserved.
+//
+
+#import "WMBaseViewController.h"
+
+@implementation WMBaseViewController
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
